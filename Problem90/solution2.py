de6a97ad99283379ef7aea16943c6c318d851f42
Vinay Ramasesh <ramasesh@berkeley.edu>
2018-12-30 16:16:20 -0600

    reorganized

--- Problem90/solution2.py
+++
@@ -1,9 +0,0 @@
-def reverse(n):
-    # returns the reverse of a number
-    reversed = 0
-    
-    while n > 0:
-        reversed = reversed * 10 + n % 10
-        n /= 10
-        
-    return reversed
