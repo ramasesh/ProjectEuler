638bea94d05b70636207aa0ef10f086367aa41d7
Vinay Ramasesh <ramasesh@berkeley.edu>
2018-12-31 01:03:27 -0600

    partial soln to 387

---
+++ Solutions/solution.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+def generate_Harshad(max_num=100):
+    """ generates a list of the right-truncatable Harshad numbers up to max_num """
+
+    RTH = [1,2,3,4,5,6,7,8,9]
+
+    if max_num > 100:
+        test_num = 10
+        if 
+
